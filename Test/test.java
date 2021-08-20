0c33ea1cc585ab7d973649bf0801de9badb180b4
Sia0 <56398189+Sia0@users.noreply.github.com>
2021-08-20 19:09:51 +0800

    Update test.java

--- Test/test.java
+++ Test/test.java
@@ -1,7 +1,10 @@
 import java.util.*;
+
 public class test {
 
 	public static void main(String[] args) {
+		ArrayList<String> strs = new ArrayList<String>();
+		strs.add("StringOne");
 		System.out.println("Hello World");
 	}
 
